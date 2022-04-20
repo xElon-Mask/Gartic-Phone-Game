b195739e2b994a3ea023ea6ff8db4162389c79b4
xElon-Mask <xelonmask@protonmail.com>
2022-04-20 16:39:39 +0200

    add function StoreWord + event newWord and stateChanged

--- contracts/GarticPhone.sol
+++ contracts/GarticPhone.sol
@@ -15,5 +15,22 @@ contract GarticPhone is Ownable {
     enum State {proposition, devine, fin}
     State state = State.proposition;
 
+    // events
+    event newWord(address player, string word);
+    event stateChanged(State enCours);
 
+    // Add new word to the list
+    function StoreWord(string memory _newWord) public {
+        require(state == State.proposition, "You can't play to the game");
+        require(keccak256(abi.encodePacked(_newWord)) != keccak256(abi.encodePacked(GetLastWord()), "Can't write same word ! "));
+        require(!played[msg.sender], "You have already played !");
+        words.push[_newWord];
+        emit newWord(msg.sender, _newWord);
+        if (words.length == 20) {
+            state = State.devine;
+            emit stateChanged(state);
+        }
+    }
+
+    // Get only the last word : GetLastWord()
 }
